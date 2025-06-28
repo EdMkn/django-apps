f6d80229ebd8b3c7c9798a4bad5f1429376da8cb
Manyim Ma Makon <nmaknyim@gmail.com>
2025-06-28 20:47:39 +0200

    Creation of eCOmmerce product variation models

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Brand, Color, Product, Size 
+from .models import Category, Brand, Color, Product, Size, ProductAttribute 
 
 admin.site.register(Category)
 admin.site.register(Brand)
@@ -9,5 +9,8 @@ admin.site.register(Size)
 class ProductAdmin(admin.ModelAdmin):
     list_display=('id','title','brand','color','size','price','status')
     list_editable=('status',)
-
 admin.site.register(Product, ProductAdmin)
+
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display=('id','product','price','color','size')
+admin.site.register(ProductAttribute, ProductAttributeAdmin)
--- bottleShop/main/models.py
+++ bottleShop/main/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+# Banner
+class Banner(models.Model):
+    img=models.CharField(max_length=200)
+    alt_text=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.title
+
 # Category
 class Category(models.Model):
     title=models.CharField(max_length=100)
     image=models.ImageField(upload_to="cat_imgs/")
+    
     def __str__(self):
         return self.title
     
@@ -11,18 +20,21 @@ class Category(models.Model):
 class Brand(models.Model):
     title=models.CharField(max_length=100)
     image=models.ImageField(upload_to="brand_imgs/")
+    
     def __str__(self):
         return self.title
 
 # Color
 class Color(models.Model):
     title=models.CharField(max_length=100)
+    
     def __str__(self):
         return self.title
     
 # Size
 class Size(models.Model):
     title=models.CharField(max_length=100)
+    
     def __str__(self):
         return self.title
 
@@ -38,5 +50,16 @@ class Product(models.Model):
     color=models.ForeignKey(Color,on_delete=models.CASCADE)
     size=models.ForeignKey(Size,on_delete=models.CASCADE)
     status=models.BooleanField(default=True)
+    
     def __str__(self):
         return self.title
+    
+# Product Attribute
+class ProductAttribute(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    color=models.ForeignKey(Color, on_delete=models.CASCADE)
+    size=models.ForeignKey(Size,on_delete=models.CASCADE)
+    price=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product.title
