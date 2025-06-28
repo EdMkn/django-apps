2de133ed7cb6407d522311e04658faf4c1e1624b
Manyim Ma Makon <nmaknyim@gmail.com>
2025-06-28 20:28:40 +0200

    Creation of Database models: - Creation of models - Registration of those models by admin.site - migration      - commands python3 manage.py makemigrations      - commands python3 manage.py migrate

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Brand, Color, Product, Size 
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id','title','brand','color','size','price','status')
+    list_editable=('status',)
+
+admin.site.register(Product, ProductAdmin)
